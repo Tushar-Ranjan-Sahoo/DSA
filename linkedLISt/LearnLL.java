1e9c326edb94a534dedc9c114c21a9b210cd69de
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2024-02-19 17:55:57 +0530

    linkedlist

---
+++ linkedLISt/LearnLL.java
@@ -0,0 +1,21 @@
+class Node {
+     int data;
+     Node next;
+     Node(int data1, Node next1){
+          this.data = data1;
+          this.next = next1;
+
+     }
+     Node(int data1){
+          this.data = data1;
+          this.next = null;
+     }
+}
+
+public class LearnLL {
+     public static void main(String[] args){
+          int [] arr = {2,5,8,7};
+          Node y = new Node(arr[0]);
+          System.out.println(y.next);
+     }
+}
---
+++ tech mahindra pratice/RecPal.java
@@ -0,0 +1,8 @@
+
+
+public class RecPal {
+static boolean sentencePalindrome(String s ){
+    char[] str = s.toCharArray();
+    Stack
+}
+}
